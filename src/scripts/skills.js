d4584c279542d21be025c5805f0606462d49cbc8
Kotov Oleg <grum1972@gmail.com>
2020-01-25 23:57:48 +0300

    Сделал виджет Скиллы

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,47 @@
-console.log('this is skills module');
+import Vue from "vue";
+
+const skill = {
+  template: "#skill",
+  props: ["skillName","skillPercent"],
+  methods: {
+    drawSkill () {
+      const circle = this.$refs["color-circle"];
+      const percent = (590 / 100) * (100 - this.skillPercent);
+      const dashArray = `${590-percent} ${percent}`;
+      circle.style.strokeDasharray = dashArray;
+    }
+
+  },
+  mounted() {
+    this.drawSkill();
+  }
+
+  
+}
+
+const skillGroup = {
+  template: "#skill-group",
+  components: {
+    skill
+  },
+  props: ["skill"]
+};
+
+new Vue({
+  el: "#skills-component",
+  template: "#skills-list",
+  data () {
+    return {
+      skills: []
+    }
+  },
+  components: {
+    skillGroup
+  },
+  
+  created() {
+    const  data =require("../data/skills.json");
+    this.skills = data;
+  }
+
+})

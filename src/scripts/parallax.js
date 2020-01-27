d2d579feaa4695a7307a3aa0e3883935fcc2cba7
Kotov Oleg <grum1972@gmail.com>
2020-01-27 22:06:06 +0300

    Сделал параллакс

---
+++ src/scripts/parallax.js
@@ -0,0 +1,21 @@
+const parallax = document.querySelector('.parallax');
+const items = parallax.children;
+console.log (items);
+function moveItemDependsOnScroll(wScroll) {
+  Array.from(items).forEach( item =>{
+    const divider = item.dataset.speed;
+    const strafe = wScroll * divider/15;
+
+    item.style.transform = `translateY(-${strafe}%)`
+  });
+}
+
+window.addEventListener ('scroll', e => {
+  const  wScroll = window.pageYOffset;
+  const element = document.querySelector('.welcome');
+  const rect = element.getBoundingClientRect();
+  if (rect.bottom>0) {
+    moveItemDependsOnScroll(wScroll)
+  }
+})
+
